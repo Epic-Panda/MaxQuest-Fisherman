86ed05894029f7363bcf5814c75cbad88f36c8c6
Darko Jakovljevic <darko.jakovljevic.998@gmail.com>
2023-11-03 20:16:51 +0100

    ui management
    level hud

---
+++ Assets/Game Assets/Scripts/UI Management/LevelHud.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LevelHud : MonoBehaviour
+{
+    [Header("General data")]
+    [SerializeField] TextMeshProUGUI m_collectedItemsAmountText;
+    [SerializeField] TextMeshProUGUI m_missedItemsAmountText;
+
+    [Header("Collected items handler")]
+    [SerializeField] int m_collectedItemsToShow;
+    [SerializeField] RectTransform m_collectedItemsContainer;
+    [SerializeField] CollectedItemController m_collectedItemPrefab;
+
+    List<CollectedItemController> m_collectedItems;
+
+    public void SetData()
+    {
+        m_collectedItemsAmountText.text = "0";
+        m_missedItemsAmountText.text = "0";
+
+        if(m_collectedItems == null)
+        {
+            m_collectedItems = new List<CollectedItemController>();
+        }
+        else
+        {
+            foreach(CollectedItemController controller in m_collectedItems)
+            {
+                controller.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void CollectItem(ItemData item, int collectedItems, int missedItems)
+    {
+        // update item collection state
+        m_collectedItemsAmountText.text = $"{collectedItems}";
+        m_missedItemsAmountText.text = $"{missedItems}";
+
+        CollectedItemController collectedItem;
+
+        // instantiate new collected item controller
+        if(m_collectedItems.Count < m_collectedItemsToShow)
+        {
+            collectedItem = Instantiate(m_collectedItemPrefab, m_collectedItemsContainer);
+        }
+        else // replace the first collected item with the new one
+        {
+            collectedItem = m_collectedItems[0];
+            m_collectedItems.RemoveAt(0);
+
+            collectedItem.gameObject.SetActive(true);
+        }
+
+        m_collectedItems.Add(collectedItem);
+
+        // reorder items
+        collectedItem.SetCollectedItem(item);
+        collectedItem.transform.SetAsFirstSibling();
+    }
+}
